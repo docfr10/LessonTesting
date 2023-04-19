34a114e907ceb016311613ecf21b4d0b480746da
docfr10 <docfr10@yandex.ru>
2023-04-19 22:23:40 +0300

    Added nullpointerexception

--- app/src/test/java/com/example/lesson_testing/CalculatorTest.kt
+++ app/src/test/java/com/example/lesson_testing/CalculatorTest.kt
@@ -1,5 +1,6 @@
 package com.example.lesson_testing
 
+import junit.framework.TestCase.assertTrue
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -17,6 +18,15 @@ class CalculatorTest {
     fun addition() {
         Assert.assertEquals(3, calculator!!.adding(1, 2).toLong())
     }
+
+    @Test(expected = NullPointerException::class)
+    fun nullStringTest() {
+        val str: String? = null
+        assertTrue(str!!.isEmpty())
+    }
+
+    @Test(timeout = 1000)
+    fun requestTest() {}
 
     @After
     fun tearDown() {
